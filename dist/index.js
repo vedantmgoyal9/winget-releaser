58406d89fbdafcbb3f1993b2ad68af30010eac0f
Vedant <83997633+vedantmgoyal2009@users.noreply.github.com>
2022-09-29 10:35:23 +0000

    Auto-update action & minor bug fixes
    Preparing action to use versioning (Resolves https://github.com/vedantmgoyal2009/winget-releaser/issues/11)
    - Added logic for auto-updating action
    - Removed fallback value for `release-repository`
      (https://github.blog/changelog/2022-09-27-github-actions-additional-information-available-in-github-event-payload-for-scheduled-workflow-runs)
    - Remove existing winget-pkgs directories (if action is run 2 times)
    - Set `GH_TOKEN` for the required steps only

--- dist/index.js
+++ dist/index.js
@@ -9699,7 +9699,8 @@ const { resolve } = __nccwpck_require__(1017);
     ...(
       await getOctokit(token).rest.repos.getReleaseByTag({
         owner: context.repo.owner,
-        repo: releaseRepository || context.repo.repo,
+        // https://github.blog/changelog/2022-09-27-github-actions-additional-information-available-in-github-event-payload-for-scheduled-workflow-runs
+        repo: releaseRepository, // || context.repo.repo,
         tag: releaseTag,
       })
     ).data,
@@ -9717,7 +9718,7 @@ const { resolve } = __nccwpck_require__(1017);
   // remove winget-pkgs directory if it exists, in case the action is run multiple times for
   // publishing multiple packages in the same workflow
   execSync(
-    `Remove-Item -Path .\\winget-pkgs\\ -Recurse -Force -ErrorAction Ignore`,
+    `If (Test-Path -Path .\\winget-pkgs\\) { Remove-Item -Path .\\winget-pkgs\\ -Recurse -Force -ErrorAction SilentlyContinue }`,
     { shell: 'pwsh', stdio: 'inherit' }
   );
   execSync(
@@ -9751,12 +9752,8 @@ const { resolve } = __nccwpck_require__(1017);
   );
   info(`::endgroup::`);
 
-  // resolve wingetdev path
+  // resolve wingetdev path (./wingetdev/wingetdev.exe)
   process.env.WINGETDEV = resolve('wingetdev', 'wingetdev.exe');
-
-  // set GH_TOKEN env variable for GitHub CLI (gh)
-  process.env.GH_TOKEN = token;
-  // https://github.com/cli/cli/blob/trunk/cmd/gh/main.go#L312
 
   info(`::group::Update manifests and create pull request`);
   const inputObject = JSON.stringify({
@@ -9778,7 +9775,91 @@ const { resolve } = __nccwpck_require__(1017);
     cwd: 'winget-pkgs/Tools',
     shell: 'pwsh',
     stdio: 'inherit',
+    env: { ...process.env, GH_TOKEN: token }, // set GH_TOKEN env variable for GitHub CLI (gh)
   });
+  info(`::endgroup::`);
+
+  info(`::group::Checking for action updates...`);
+  // check if action version is a version (starts with `v`) and not a pinned commit ref
+  if (/^v\d+$/g.test(process.env.GITHUB_ACTION_REF)) {
+    const latestVersion = (
+      await getOctokit(token).rest.repos.getLatestRelease({
+        owner: 'vedantmgoyal2009',
+        repo: 'winget-releaser',
+      })
+    ).data.tag_name;
+
+    info(`Current action version: ${process.env.GITHUB_ACTION_REF}`);
+    info(`Latest version found: ${latestVersion}`);
+
+    // if the latest version is greater than the current version, then update the action
+    if (latestVersion > currentVersion) {
+      execSync(
+        `git clone https://x-access-token:${token}@github.com/${process.env.GITHUB_REPOSITORY}.git`,
+        {
+          stdio: 'inherit',
+        }
+      );
+      execSync(`git config --local user.name github-actions`, {
+        stdio: 'inherit',
+        cwd: process.env.GITHUB_REPOSITORY.split('/')[1],
+      });
+      execSync(
+        `git config --local user.email 41898282+github-actions[bot]@users.noreply.github.com`,
+        { stdio: 'inherit', cwd: process.env.GITHUB_REPOSITORY.split('/')[1] }
+      );
+      // replace the version in the workflow file using `find` and `sed`
+      execSync(
+        `find -name '*.yml' -or -name '*.yaml' -exec sed -i 's/vedantmgoyal2009\\/winget-releaser@${process.env.GITHUB_ACTION_REF}/vedantmgoyal2009\\/winget-releaser@${latestVersion}/g' {} +`,
+        {
+          stdio: 'inherit',
+          cwd: `${
+            process.env.GITHUB_REPOSITORY.split('/')[1]
+          }/.github/workflows`,
+          shell: 'bash',
+        }
+      );
+      // create a new branch, commit and push the changes, and create a pull request
+      execSync(
+        `git commit --all -m \"ci(winget-releaser): update action from ${process.env.GITHUB_ACTION_REF} to ${latestVersion}\"`,
+        {
+          stdio: 'inherit',
+          cwd: process.env.GITHUB_REPOSITORY.split('/')[1],
+        }
+      );
+      execSync(`git switch -c winget-releaser/update-to-${latestVersion}`, {
+        stdio: 'inherit',
+        cwd: process.env.GITHUB_REPOSITORY.split('/')[1],
+      });
+      execSync(
+        `git push --force-with-lease --set-upstream origin winget-releaser/update-to-${latestVersion}`,
+        {
+          stdio: 'inherit',
+          cwd: process.env.GITHUB_REPOSITORY.split('/')[1],
+        }
+      );
+      execSync(
+        `@\"
+This PR was automatically created by the [WinGet Releaser GitHub Action](https://github.com/vedantmgoyal2009/winget-releaser) to update the action version from \`${process.env.GITHUB_ACTION_REF}\` to \`${newVersion}\`.\`r\`n
+The auto-update function help maintainers keep their workflows up-to-date with the latest version of the action.\`r\`n
+You can close this pull request if you don't want to update the action version.\`r\`n
+Mentioning @vedantmgoyal2009 for a second pair of eyes, in case any breaking changes have been introduced in the new version of the action.
+\"@ | gh pr create --fill --body-file -`,
+        {
+          stdio: 'inherit',
+          cwd: process.env.GITHUB_REPOSITORY.split('/')[1],
+          shell: 'pwsh',
+          env: { ...process.env, GH_TOKEN: token }, // set GH_TOKEN env variable for GitHub CLI (gh)
+        }
+      );
+    } else {
+      info(`No updates found. Bye bye!`);
+    }
+  } else {
+    info(
+      `The workflow maintainer has pinned the action to a commit ref. Skipping update check...`
+    );
+  }
   info(`::endgroup::`);
 })();
 
